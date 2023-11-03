e874ac7c5419fe8f8989a839ba4e2a7d6260c2ce
CJMario89 <a7654321000@gmail.com>
2023-11-03 12:21:28 +0800

    add reference_link column to database

---
+++ migrations/20231103041849_add_image_link_to_reference/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "cityImage" ADD COLUMN     "referenceLink" TEXT NOT NULL DEFAULT '';
+
+-- AlterTable
+ALTER TABLE "spotImage" ADD COLUMN     "referenceLink" TEXT NOT NULL DEFAULT '';
