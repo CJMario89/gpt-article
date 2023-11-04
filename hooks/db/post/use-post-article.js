f7d2196c954a9bb9a4356fdb945d47ec55e9c21d
CJMario89 <a7654321000@gmail.com>
2023-11-04 11:38:06 +0800

    chore: throw err to useMutation in mutation

--- hooks/db/post/use-post-article.js
+++ hooks/db/post/use-post-article.js
@@ -5,13 +5,9 @@ import { requestCityPhotoQuery } from "service/google-query";
 const usePostArticle = (option) => {
   return useMutation(
     async ({ country, city, article }) => {
-      try {
-        await requestCityPhotoQuery({ city });
-        const result = await postCityArticle({ country, city, article });
-        return result.json();
-      } catch (e) {
-        console.log(e);
-      }
+      await requestCityPhotoQuery({ city });
+      const result = await postCityArticle({ country, city, article });
+      return result.json();
     },
     { ...option }
   );
--- hooks/db/post/use-post-cities.js
+++ hooks/db/post/use-post-cities.js
@@ -4,12 +4,8 @@ import { postCities } from "service/db-query";
 const usePostCities = (options) => {
   return useMutation(
     async ({ country, cities }) => {
-      try {
-        const result = await postCities({ country, cities });
-        return result.json();
-      } catch (e) {
-        console.log(e);
-      }
+      const result = await postCities({ country, cities });
+      return result.json();
     },
     { ...options }
   );
