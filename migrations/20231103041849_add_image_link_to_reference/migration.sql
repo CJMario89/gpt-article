475d03564a32327dae4e03f6171975e47d859bd7
CJMario89 <a7654321000@gmail.com>
2023-11-06 22:32:36 +0800

    change database to sqlite

--- migrations/20231103041849_add_image_link_to_reference/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "cityImage" ADD COLUMN     "referenceLink" TEXT NOT NULL DEFAULT '';
-
--- AlterTable
-ALTER TABLE "spotImage" ADD COLUMN     "referenceLink" TEXT NOT NULL DEFAULT '';
