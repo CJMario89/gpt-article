5fbc35b20134e5588c51b5ec14b456cf0598c92f
CJMario89 <a7654321000@gmail.com>
2023-11-15 17:54:07 +0800

    fix colormode andd promptText

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,15 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { useEffect } from "react";
 import theme from "theme";
 import { Fonts } from "theme/component";
 
 const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    localStorage.setItem("chakra-ui-color-mode", "light");
+  }, []);
   return (
     <ChakraProvider theme={theme}>
       <Fonts />
